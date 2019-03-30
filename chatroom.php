b9081317e3eebfc5606b0f9d2cdc3ae7a4a36c40
Jing <jingkcheng@hotmail.com>
2019-03-29 21:01:37 -0400

    Added styling to chatroom

--- chatroom.php
+++ chatroom.php
@@ -5,15 +5,34 @@ $file = "chatroom".$id.".xml";
 $chatroom = simplexml_load_file($file);
 $messageCount = $chatroom->chat->user->count();
 // echo $messageCount;
+?>
 
-foreach($chatroom->chat->user as $user)
-{
-    echo "</br>";
-    echo $user->name;
-    echo "</br>";
-    echo $user->message->submissionTime;
-    echo "</br>";
-    echo $user->message->text;
-    echo "</br>";
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+<div id="chatroom">
+      <div id="chats">
+        <?php
+        foreach($chatroom->chat->user as $user)
+        {
+            echo "</br>";
+            echo $user->name;
+            echo "</br>";
+            echo $user->message->submissionTime;
+            echo "</br>";
+            echo $user->message->text;
+            echo "</br>";
+        }
+        ?>
+      </div>
+</div>
+<form>
+    <input type="text" name="messages" id="message">
+    <button type="submit" id="submitButton">Send</button>
+</form>
 
-}
+</body>
+</html>
