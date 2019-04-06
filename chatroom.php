cbb8142b7b6fc7274f7dbf7392f4659c9d9d5b06
Jing <jingkcheng@hotmail.com>
2019-04-06 18:45:06 -0400

    Begin styling chatroom.php

--- chatroom.php
+++ chatroom.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <link rel="stylesheet" type="text/css" href="css/chatroom.css">
     <link rel="stylesheet" type="text/css" href="css/global.css">
@@ -10,12 +11,13 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
     <script src="scripts/refreshChat.js"></script>
-    <script src="scripts/submitMessage.js"></script>    
+    <script src="scripts/submitMessage.js"></script>
 </head>
 <?php
 include 'header.php';
 include 'sendMessage.php';
 ?>
+
 <body>
     <div id="chatroomContainer">
         <div id="chatroom">
@@ -32,18 +34,27 @@ include 'sendMessage.php';
                 ?>
             </div>
         </div>
-            <form action='chatroom.php' method='post' id="submitMessageForm">
-            <input type="text" name="messages" id="message">
+        <form action='chatroom.php' method='post' id="submitMessageForm">
+            <div class="input-field col s6">
+                
+                <textarea id="message" name="messages" class="materialize-textarea"></textarea>
+                <label for="textarea1">Type your message here.</label>
+                <button id="sendButton" class="btn waves-effect waves-light" type="submit" name="submitMessage">Send
+                <i class="material-icons right">send</i>
+                <!-- <input type="text" name="messages" id="message">
+                <label for="messages">Type your message here.</label> -->
+            </div>
             <input type="hidden" name="id" id="sessionId" value="<?php echo $_SESSION['id']?>" />
             <input type="hidden" name="file" id="sessionFile" value="<?php echo $_SESSION['file']?>" />
-            <button type="submit" name="submitMessage" id="submitButton">Send</button>
+            </button>
         </form>
-        <form action="chatrooms.php">
-            <button type="submit" name="back">Back to chatroom list</button>
-        </form>
+        <button class="btn waves-effect waves-light" onclick="window.location.href='chatrooms.php';">Back to list
+            <i class="material-icons left">arrow_back</i>
+        </button>
     </div>
 </body>
 <?php
 include 'footer.php';
 ?>
+
 </html>
